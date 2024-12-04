c17c4c7bf7a87fa68586bbb762d818cfef940473
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-04 14:50:36 +0530

    Add files via upload

---
+++ smallestamg3.java
@@ -0,0 +1,21 @@
+public class smallestamg3
+{
+    public static void main(String[] args)
+    {
+        int a=34;
+        int b=64;
+        int c=23;
+        if(a<b&&a<c)
+        {
+            System.out.println("A is bigger");
+        }
+        else if(b<a&&b<c)
+        {
+            System.out.println("B is bigger");
+        }
+        else
+        {
+            System.out.println("C is bigger");
+        }
+    }
+}
