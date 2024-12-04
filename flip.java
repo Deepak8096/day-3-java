67091d41d4d0463aec7f1d4c2b8cbe9302b34efd
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-04 12:38:54 +0530

    Add files via upload

---
+++ flip.java
@@ -0,0 +1,17 @@
+public class flip
+{
+    public static void main(String[] args)
+    {
+        int a=10;
+        int pos=1;
+        if((a&pos)==1)
+        {
+            System.out.println("set bit");
+        }
+        else
+        {
+            System.out.println("not a set bit");
+        }
+    
+    }
+}
