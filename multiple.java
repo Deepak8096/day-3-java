c01d5467ffe5a13afdedbb136388ab192bce1e17
Deepak8096 <archakamdeepak81@gmail.com>
2024-12-04 12:39:08 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,16 @@
+public class multiple
+{
+    public static void main(String[] args)
+    {
+        int a=16;
+        if((a&(a-2))==0)
+        {
+            System.out.println("multiple of 2");
+        }
+        else
+        {
+            System.out.println("not a multiple of 2");
+        }
+    
+    }
+}
